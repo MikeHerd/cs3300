1a330cf0933c185ff416a38d017fae7e18fa85fb
MikeHerd <wherd@uccs.edu>
2022-04-07 05:36:25 +0000

    placed filters at the top of the page so that the filters actually loaded. Code coverage now up to 74.07% from 67.8%

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __dir__)
-
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/bin/'
@@ -18,6 +8,17 @@ SimpleCov.start 'rails' do
   add_filter '/spec/'
   add_filter '/tmp/'
 end
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
